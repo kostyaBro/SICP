e7199fea6317683012eee2efdcfa51ae3427ba7e
Kostya <indianor201203@gmail.com>
2019-04-08 16:05:16 +0500

    Expression Impl in Line Reader

--- interpreter/LineReader/LineReader.go
+++ interpreter/LineReader/LineReader.go
@@ -1,8 +1,11 @@
 package LineReader
 
 import (
+	"fmt"
 	"github.com/kostyaBro/SICP/interpreter/interfaces"
+	"github.com/pkg/errors"
 	"io"
+	"reflect"
 )
 
 type lineReader struct {
@@ -14,19 +17,87 @@ func New(heep interfaces.IHeep) interfaces.ILisp {
 }
 
 func (lr *lineReader) Add(args ...interface{}) (out interface{}, err error) {
-	panic("implement me")
+	if len(args) == 0 {
+		return 0, nil
+	}
+	if reflect.ValueOf(args[0]).Kind() >= reflect.Int && reflect.ValueOf(args[0]).Kind() <= reflect.Uint64 {
+		var answer int64
+		for _, arg := range args {
+			answer += reflect.ValueOf(arg).Int()
+		}
+		return answer, nil
+	}
+	if reflect.ValueOf(args[0]).Kind() == reflect.Float32 || reflect.ValueOf(args[0]).Kind() == reflect.Float64 {
+		var answer float64
+		for _, arg := range args {
+			answer += reflect.ValueOf(arg).Float()
+		}
+		return answer, nil
+	}
+	return 0, errors.New(fmt.Sprintf("args not work %+v", args))
 }
 
 func (lr *lineReader) Rem(args ...interface{}) (out interface{}, err error) {
-	panic("implement me")
+	if len(args) == 0 {
+		return 0, nil
+	}
+	if reflect.ValueOf(args[0]).Kind() >= reflect.Int && reflect.ValueOf(args[0]).Kind() <= reflect.Uint64 {
+		var answer int64
+		for _, arg := range args {
+			answer -= reflect.ValueOf(arg).Int()
+		}
+		return answer, nil
+	}
+	if reflect.ValueOf(args[0]).Kind() == reflect.Float32 || reflect.ValueOf(args[0]).Kind() == reflect.Float64 {
+		var answer float64
+		for _, arg := range args {
+			answer -= reflect.ValueOf(arg).Float()
+		}
+		return answer, nil
+	}
+	return 0, errors.New(fmt.Sprintf("args not work %+v", args))
 }
 
 func (lr *lineReader) Mul(args ...interface{}) (out interface{}, err error) {
-	panic("implement me")
+	if len(args) == 0 {
+		return 0, nil
+	}
+	if reflect.ValueOf(args[0]).Kind() >= reflect.Int && reflect.ValueOf(args[0]).Kind() <= reflect.Uint64 {
+		var answer int64
+		for _, arg := range args {
+			answer *= reflect.ValueOf(arg).Int()
+		}
+		return answer, nil
+	}
+	if reflect.ValueOf(args[0]).Kind() == reflect.Float32 || reflect.ValueOf(args[0]).Kind() == reflect.Float64 {
+		var answer float64
+		for _, arg := range args {
+			answer *= reflect.ValueOf(arg).Float()
+		}
+		return answer, nil
+	}
+	return 0, errors.New(fmt.Sprintf("args not work %+v", args))
 }
 
 func (lr *lineReader) Div(args ...interface{}) (out interface{}, err error) {
-	panic("implement me")
+	if len(args) == 0 {
+		return 0, nil
+	}
+	if reflect.ValueOf(args[0]).Kind() >= reflect.Int && reflect.ValueOf(args[0]).Kind() <= reflect.Uint64 {
+		var answer int64
+		for _, arg := range args {
+			answer /= reflect.ValueOf(arg).Int()
+		}
+		return answer, nil
+	}
+	if reflect.ValueOf(args[0]).Kind() == reflect.Float32 || reflect.ValueOf(args[0]).Kind() == reflect.Float64 {
+		var answer float64
+		for _, arg := range args {
+			answer /= reflect.ValueOf(arg).Float()
+		}
+		return answer, nil
+	}
+	return 0, errors.New(fmt.Sprintf("args not work %+v", args))
 }
 
 func (lr *lineReader) Define(name string, body string) (ok bool, err error) {
